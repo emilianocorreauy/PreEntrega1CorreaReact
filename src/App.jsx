c56f4c38203818533bcd618389fd0dc39f8e5820
emilianocorreauy <emiliano.correa9@gmail.com>
2024-02-22 03:00:01 -0300

    finalizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from './components/Navbar/Navbar.jsx'
+import Navbar from './components/Navbar/Navbar.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainers/ItemListContainers.jsx';
 
